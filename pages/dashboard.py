ddc513c9173e932daad9296d92158780a6092d73
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-09-28 12:09:19 +0530

    Added graph options, and added polts

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -20,93 +20,228 @@ df_pie = pd.DataFrame(
     }
 )
 
-# graph options
-# graph_options = [
-#     html.Div(
-#         [
-#             html.Label(['Choose a graph:'],style={'font-weight': 'bold'}),
-#             dcc.Dropdown(
-#                 id='dropdown',
-#                 options=[
-#                     {'label': 'refresh chart', 'value': 'chart refresh'},
-#                     {'label': 'Export Data to Excel', 'value': 'data download'},
-#                     {'label': 'Delete chart', 'value': 'delete chart'}
-#                 ],
-#                 value='graph1',
-#                 style={"width": "60%"}
-#             )
-#         ]
-#     )
-# ]
-
-dashboard = [
-    navbar,
-    html.Br(),
-    dbc.Row(
-        dbc.Col(
-            html.H3(
-                "Dashboard Page", 
-                style={"color": "#a3004c", "font-family": "changa"}
-            ),
-            width=2,
+# Create a list of options for the dropdown menu
+dropdown = html.Div(
+    [
+        dbc.DropdownMenu(
+            [
+                dbc.Col(
+                    dbc.DropdownMenuItem(
+                        "Update Chart", id="update-button", n_clicks=0
+                    )
+                ),
+                dbc.Col(
+                    dbc.DropdownMenuItem(
+                        "Extract data", id="extract-button", n_clicks=0
+                    )
+                )
+            ],
+            label="Chart Options",
         ),
-        className="ml-5 mb-0",
-    ),
-    
-    dbc.Row(
-        [
-            dbc.Col(
-                dbc.Card(
-                    dbc.CardBody(
-                        [
-                            # Line chart
-                            dcc.Graph(
-                                id='line-chart',
-                                figure={
-                                    'data': [
-                                        {'x': df_line['X'], 'y': df_line['Y'], 'type': 'line', 'name': 'Line Chart'},
-                                    ],
-                                    'layout': {
-                                        'title': 'Line Chart'
-                                    }
-                                },
-                                config={
-                                    'displayModeBar': False
-                                },
-                            )
-                        ]
-                    ),
-                    className="mb-3"
-                ),
-                width=6,
-                style={"margin-left": "50px"}
-            ),
 
-            dbc.Col(
-                dbc.Card(
-                    dbc.CardBody(
+        # Optional
+        html.P(
+            id="item-clicks", 
+            className="mt-3"
+        ),
+    ]
+)
+
+dashboard = dbc.Container(
+    [
+        navbar,
+        html.Br(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.Card(
                         [
-                            dcc.Graph(
-                                id='pie-chart',
-                                figure={
-                                    'data': [
-                                        {'labels': df_pie['Category'], 'values': df_pie['Values'], 'type': 'pie', 'name': 'Pie Chart'},
+                            dbc.CardHeader(
+                                dbc.Row(
+                                    [
+                                        dbc.Col(
+                                            html.H5("Site Stats"), 
+                                            width=6
+                                        ),
+                                        dbc.Col(
+                                            dropdown,
+                                            width=6,
+                                            style={"textAlign": "right"},
+                                        ),  # Button
                                     ],
-                                    'layout': {
-                                        'title': 'Pie Chart'
-                                    }
-                                },
-                                config={
-                                    'displayModeBar': False
-                                }
-                            )
-                        ]
+                                    style={"marginLeft": "0px", "marginRight": "0px"}
+                                ),
+                            ),
+                            dbc.CardBody(
+                                [
+                                    # Line chart
+                                    dcc.Graph(
+                                        id='line-chart',
+                                        figure={
+                                            'data': [
+                                                {'x': df_line['X'], 'y': df_line['Y'], 'type': 'line', 'name': 'Line Chart'},
+                                            ],
+                                            'layout': {
+                                                'title': 'Line Chart'
+                                            }
+                                        },
+                                        config={
+                                            'displayModeBar': False
+                                        },
+                                    ),
+                                    dcc.Markdown(id="button-output"),
+                                ]
+                            ),
+                        ],
+                        className="mb-3"
                     ),
+                    width=6,
+                    style={"margin-left": "50px"}
+                ),
+                
+                dbc.Col(
+                dbc.Card(
+                    [
+                        dbc.CardHeader(
+                            dbc.Row(
+                                [
+                                    dbc.Col(
+                                        html.H5("Devices"), 
+                                        width=6
+                                    ),
+                                    dbc.Col(
+                                        dropdown,
+                                        width=6,
+                                        style={"textAlign": "right"},
+                                    ),
+                                ],
+                                style={"marginLeft": "0px", "marginRight": "0px", "marginBottom": "0px"}
+                            ),
+                        ),
+                        dbc.CardBody(
+                            [
+                                # Line chart
+                                dcc.Graph(
+                                    id='pie-chart',
+                                    figure={
+                                        'data': [
+                                            {'labels': df_pie['Category'], 'values': df_pie['Values'], 'type': 'pie', 'name': 'Pie Chart'},
+                                        ],
+                                        'layout': {
+                                            'title': 'Pie Chart'
+                                        }
+                                    },
+                                    config={
+                                        'displayModeBar': False
+                                    },
+                                ),
+                                dcc.Markdown(id="button-output"),
+                            ]
+                        ),
+                    ],
                     className="mb-3"
                 ),
-                width=5
+                width=5,
             )
-        ]
-    )
-]
-
+            ]
+        ),
+        
+        
+        
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.Card(
+                        [
+                            dbc.CardHeader(
+                                dbc.Row(
+                                    [
+                                        dbc.Col(
+                                            html.H5("Site Stats"), 
+                                            width=6
+                                        ),
+                                        dbc.Col(
+                                            dropdown,
+                                            width=6,
+                                            style={"textAlign": "right"},
+                                        ),  # Button
+                                    ],
+                                    style={"marginLeft": "0px", "marginRight": "0px"}
+                                ),
+                            ),
+                            dbc.CardBody(
+                                [
+                                    # Line chart
+                                    dcc.Graph(
+                                        id='line-chart',
+                                        figure={
+                                            'data': [
+                                                {'x': df_line['X'], 'y': df_line['Y'], 'type': 'line', 'name': 'Line Chart'},
+                                            ],
+                                            'layout': {
+                                                'title': 'Line Chart'
+                                            }
+                                        },
+                                        config={
+                                            'displayModeBar': False
+                                        },
+                                    ),
+                                    dcc.Markdown(id="button-output"),
+                                ]
+                            ),
+                        ],
+                        className="mb-3"
+                    ),
+                    width=6,
+                    style={"margin-left": "50px"}
+                ),
+                
+                dbc.Col(
+                dbc.Card(
+                    [
+                        dbc.CardHeader(
+                            dbc.Row(
+                                [
+                                    dbc.Col(
+                                        html.H5("Devices"), 
+                                        width=6
+                                    ),
+                                    dbc.Col(
+                                        dropdown,
+                                        width=6,
+                                        style={"textAlign": "right"},
+                                    ),
+                                ],
+                                style={"marginLeft": "0px", "marginRight": "0px", "marginBottom": "0px"}
+                            ),
+                        ),
+                        dbc.CardBody(
+                            [
+                                # Line chart
+                                dcc.Graph(
+                                    id='pie-chart',
+                                    figure={
+                                        'data': [
+                                            {'labels': df_pie['Category'], 'values': df_pie['Values'], 'type': 'pie', 'name': 'Pie Chart'},
+                                        ],
+                                        'layout': {
+                                            'title': 'Pie Chart'
+                                        }
+                                    },
+                                    config={
+                                        'displayModeBar': False
+                                    },
+                                ),
+                                dcc.Markdown(id="button-output"),
+                            ]
+                        ),
+                    ],
+                    className="mb-3"
+                ),
+                width=5,
+            )
+            ]
+        )
+    ],
+    fluid=True
+)
